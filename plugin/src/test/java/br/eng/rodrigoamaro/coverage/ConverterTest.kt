19f312ff1d9cc1f3997f0e4321390565036e341a
Rodrigo Amaro <ramaro@uolinc.com>
2018-12-06 14:43:51 -0200

    Commit with no source changes to generate another diff

--- plugin/src/test/java/br/eng/rodrigoamaro/coverage/ConverterTest.kt
+++ plugin/src/test/java/br/eng/rodrigoamaro/coverage/ConverterTest.kt
@@ -2,15 +2,16 @@ package br.eng.rodrigoamaro.coverage
 
 import org.gradle.internal.impldep.org.junit.runner.RunWith
 import org.gradle.internal.impldep.org.junit.runners.JUnit4
+import org.junit.Assert
 import org.junit.Test
 import java.io.File
-import java.io.FileFilter
+import java.math.BigDecimal
 
 
 @RunWith(JUnit4::class)
 class ConverterTest {
 
-    private val classPath = File(javaClass.classLoader.getResource("kotlin-classes/debug").file)
+    private val classPath = File(javaClass.classLoader.getResource("classes").file)
     private val sourcePath = File("../../sample/src/main")
     private val destination = File("./out/report")
 
@@ -19,17 +20,17 @@ class ConverterTest {
         val analyser = CoverageDataAnalyser(FilteredCoverageBuilder(), listOf(classPath))
         val converter = Converter(analyser, sourcePath, destination)
         converter.convert(
-            File(javaClass.classLoader.getResource("").file)
-                .listFiles(FileFilter { it.name.endsWith(".ec") }).toList()
+            listOf(File(javaClass.classLoader.getResource("test.exec").file))
         )
     }
 
     @Test
-    fun deltaCoverage() {
+    fun deltaCoverageAllFilesPresent() {
         val coverageData = CoverageData(javaClass.classLoader.getResource("merged.xml").file)
         val diffCoverage = DiffCoverage(javaClass.classLoader.getResource("diff-file").file, coverageData)
         val coverage = diffCoverage.calculate()
-        println("Coverage: $coverage")
+        Assert.assertEquals(coverage, BigDecimal(93).setScale(1))
     }
+
 
 }
