eaa3572f071ed4e1d46d68d664cfcb6f3d5f6740
Rodrigo Amaro <ramaro@uolinc.com>
2018-12-06 20:10:08 -0200

    Removing necessity to apply Jacoco plugin

--- plugin/src/main/kotlin/br/eng/rodrigoamaro/coverage/CoveragePlugin.kt
+++ plugin/src/main/kotlin/br/eng/rodrigoamaro/coverage/CoveragePlugin.kt
@@ -2,6 +2,7 @@ package br.eng.rodrigoamaro.coverage
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 
 class CoveragePlugin : Plugin<Project> {
     companion object {
@@ -11,9 +12,11 @@ class CoveragePlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.extensions.create("coverage", CoverageExtension::class.java, project)
+        project.pluginManager.apply("jacoco")
+        (project.extensions.getByName("jacoco") as JacocoPluginExtension).toolVersion = "0.8.2"
         project.tasks.create(REFINE_TASK, CoverageRefinerTask::class.java)
         project.tasks.create(DELTA_COVERAGE_TASK, DeltaCoverageTask::class.java) {
-            it.mustRunAfter(REFINE_TASK)
+            it.dependsOn(REFINE_TASK)
         }
     }
 }
